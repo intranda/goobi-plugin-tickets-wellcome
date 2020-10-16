9f5afd1f753c901519adf8af92c2d74e8a536842
Robert Sehr <robert.sehr@intranda.com>
2020-10-16 14:58:20 +0200

    added audio handler

--- wellcome-mq-tickets/src/main/java/org/goobi/api/mq/ImportAudioDataHandler.java
+++ wellcome-mq-tickets/src/main/java/org/goobi/api/mq/ImportAudioDataHandler.java
@@ -2,11 +2,8 @@ package org.goobi.api.mq;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
-import org.goobi.beans.Process;
-import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginReturnValue;
 
 import com.amazonaws.AmazonClientException;
@@ -18,11 +15,7 @@ import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 
 import de.sub.goobi.config.ConfigurationHelper;
-import de.sub.goobi.helper.CloseStepHelper;
 import de.sub.goobi.helper.S3FileUtils;
-import de.sub.goobi.helper.StorageProvider;
-import de.sub.goobi.helper.enums.StepStatus;
-import de.sub.goobi.persistence.managers.ProcessManager;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -33,14 +26,16 @@ import lombok.extern.log4j.Log4j2;
  */
 
 @Log4j2
-public class ImportVideoDataHandler implements TicketHandler<PluginReturnValue> {
+public class ImportAudioDataHandler implements TicketHandler<PluginReturnValue> {
 
     @Override
     public PluginReturnValue call(TaskTicket ticket) {
         String bucket = ticket.getProperties().get("bucket");
 
         String s3Key = ticket.getProperties().get("s3Key");
-        Path destinationFolder = Paths.get(ticket.getProperties().get("destination"));
+        Path destinationFolder
+        = Paths.get(ticket.getProperties().get("destination"));
+
 
         log.debug("copy {} to {}", bucket + "/" + s3Key, destinationFolder);
 
@@ -55,68 +50,15 @@ public class ImportVideoDataHandler implements TicketHandler<PluginReturnValue> 
             destinationFile = destinationFolder.resolve(s3Key);
         }
 
+
+
         Copy copy = transferManager.copy(bucket, s3Key, ConfigurationHelper.getInstance().getS3Bucket(), S3FileUtils.path2Key(destinationFile));
         try {
             copy.waitForCompletion();
         } catch (AmazonClientException | InterruptedException e) {
             log.error(e);
         }
-        // check if the upload is complete
-        List<String> filenamesInFolder = StorageProvider.getInstance().list(destinationFolder.toString());
-        boolean posterFound = false;
-        boolean mpegFound = false;
-        boolean mp4Found = false;
-        boolean mxfFound = false;
-        // TODO set pdfFound to false to activate the pdf import
-        boolean pdfFound = true;
 
-        for (String filename : filenamesInFolder) {
-            String suffix = filename.substring(filename.indexOf(".") + 1);
-            switch (suffix) {
-
-                case "jpg":
-                case "JPG":
-                case "jpeg":
-                case "JPEG":
-                    posterFound = true;
-                    break;
-                case "mpg":
-                case "MPG":
-                case "mpeg":
-                case "MPEG":
-                    mpegFound = true;
-                    break;
-                case "mp4":
-                case "MP4":
-                    mp4Found = true;
-                    break;
-                case "mxf":
-                case "MXF":
-                    mxfFound = true;
-                    break;
-                case "pdf":
-                case "PDF":
-                    pdfFound = true;
-                    break;
-            }
-
-            // upload is complete, if poster + mpg or poster + mp4 + mxf are available
-            if (posterFound && pdfFound && ((mpegFound) || (mp4Found && mxfFound))) {
-                // close current task
-                Process process = ProcessManager.getProcessById(ticket.getProcessId());
-                Step stepToClose = null;
-
-                for (Step processStep : process.getSchritte()) {
-                    if (processStep.getBearbeitungsstatusEnum() == StepStatus.OPEN || processStep.getBearbeitungsstatusEnum() == StepStatus.INWORK) {
-                        stepToClose = processStep;
-                        break;
-                    }
-                }
-                if (stepToClose != null) {
-                    CloseStepHelper.closeStep(stepToClose, null);
-                }
-            }
-        }
         String deleteFiles = ticket.getProperties().get("deleteFiles");
         if (StringUtils.isNotBlank(deleteFiles) && deleteFiles.equalsIgnoreCase("true")) {
             s3.deleteObject(bucket, s3Key);
