016d08c1f2828c3130985f6f5bbb19edf838e153
Robert Sehr <robert.sehr@intranda.com>
2025-06-06 06:42:19 +0200

    properties

--- module-job/src/main/java/org/goobi/api/mq/ImportEPHandler.java
+++ module-job/src/main/java/org/goobi/api/mq/ImportEPHandler.java
@@ -413,8 +413,6 @@ public class ImportEPHandler implements TicketHandler<PluginReturnValue> {
 
         BeanHelper bHelper = new BeanHelper();
         bHelper.SchritteKopieren(template, process);
-        bHelper.ScanvorlagenKopieren(template, process);
-        bHelper.WerkstueckeKopieren(template, process);
         bHelper.EigenschaftenKopieren(template, process);
 
         return process;
